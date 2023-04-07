fa79f9a86aed94ce29b39494d909a42520084a39
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-04-08 02:33:13 +0300

    Выполнены задачи семинара №3 и ДЗ №3

---
+++ ДЗ №3/2.js
@@ -0,0 +1,7 @@
+function greeting (name)
+{
+    alert(`Здравствуй, ${name}!`)
+}
+
+const namePerson = prompt('Как Вас зовут?: ')
+greeting(namePerson)
---
+++ Семинар 3/main.js
@@ -0,0 +1,16 @@
+// Задача 2. Получить от пользователя два числа и вывести в диалоговое окно сумму значений, которые ввел пользователь.
+// Вывод должен выглядеть так:
+// Результат сложения чисел 5 и 2 равен 7.
+
+// const num = Number.parseFloat(prompt("Введите первое число: "))
+// const num2 = Number.parseFloat(prompt("Введите первое число: "))
+// alert(`Результат сложения чисел ${num} и ${num2} равен ${sum(num, num2).toFixed(2)}`)
+
+// function sum(number1, number2)
+// {
+//     return number1+number2
+// }
+
+const answer = confirm('Вам хорошо живется?');
+if (answer == true) alert('Тогда мы идем к Вам')
+else alert('Ну Вы держитесь там')
