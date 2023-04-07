3cc72a8d10bc366f1d5951028993d33f8e349a12
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-04-08 02:36:30 +0300

    испрвление 1.js

--- ДЗ №3/1.js
+++ ДЗ №3/1.js
@@ -1,3 +1,3 @@
 const temperatureCelsius = Number.parseFloat(prompt('Введите температуру в градусах Цельсия'))
-let temperatureFahrenheit = (9*temperatureCelsius/5) + 32
+const temperatureFahrenheit = (9*temperatureCelsius/5) + 32
 alert(`Цельсий: ${temperatureCelsius}, Фаренгейт: ${temperatureFahrenheit.toFixed(1)}`)
